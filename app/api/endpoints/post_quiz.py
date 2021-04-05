7c5c2b86b17878a4d1d73d53e8036cdfae054ffd
BoroviyOrest <Borovyorest@gmail.com>
2021-04-06 00:11:22 +0300

    add dependency for get_quiz_by_post_id controller

--- app/api/endpoints/post_quiz.py
+++ app/api/endpoints/post_quiz.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 
+from core.dependencies import init_crud
 from crud.quiz import QuizCRUD
 from models.quiz import QuizInResponsePartial
 
@@ -7,8 +8,7 @@ router = APIRouter()
 
 
 @router.get('/', response_model=QuizInResponsePartial)
-async def get_quiz(request: Request, post_id: int):
-    crud = QuizCRUD(request.app.state.mongodb)
+async def get_quiz_by_post_id(post_id: int, crud: QuizCRUD = Depends(init_crud(QuizCRUD))):
     quiz = await crud.get_by_post_id(post_id)
 
     return quiz
